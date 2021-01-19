c0c108f2a351044efb5fd532351d2c88463baa0b
MarcosVAdS <victor.kernel14@gmail.com>
2021-01-18 23:08:40 -0300

    criacao de login e logout

--- t10/user/models.py
+++ t10/user/models.py
@@ -1,7 +1,3 @@
 from django.db import models
 
 # Create your models here.
-class User():
-    login = models.CharField(max_length=11, blank=False)
-    password = models.CharField(max_length=8, blank=False)
-    
--- t10/user/views.py
+++ t10/user/views.py
@@ -1,8 +1,16 @@
-from django.shortcuts import render, redirect
-from django.contrib.auth import authenticate, login, logout
-
+from django.shortcuts import render
+from django.http import HttpResponse
+from django.contrib.auth import authenticate, login
 # Create your views here.
+
 def login(request):
+    username = request.POST['username']
+    password = request.POST['password']
+    user = authenticate(request, username=username, password=password)
+    if user is not None:
+        return HttpResponse("Login!")
+    else:
+        return HttpResponse("Ops, error")
 
 def logout(request):
-    
+    return HttpResponse("Ops, logout")
