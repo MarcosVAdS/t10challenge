b451224beab251cc1d631ab0211f3629459d81eb
MarcosVAdS <victor.kernel14@gmail.com>
2021-01-19 19:34:34 -0300

    user model, debit functions

--- t10/debit/views.py
+++ t10/debit/views.py
@@ -1,3 +1,18 @@
 from django.shortcuts import render
+from django.http import HttpResponse
 
 # Create your views here.
+def new_request():
+    return HttpResponse("New Request")
+
+def cancel_request():
+    return HttpResponse("Cancel Request")
+
+def approve_request():
+    return HttpResponse("Aproved request")
+
+def reject_request():
+    return HttpResponse("Rejected Request")
+
+def list_request():
+    return HttpResponse("All Requests")
